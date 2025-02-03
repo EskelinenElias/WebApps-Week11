24baf942b95ee7af9b26baa637923a9f3b451487
Elias Eskelinen <104145838+EskelinenElias@users.noreply.github.com>
2025-02-03 15:27:15 +0200

    Task 2. removed useref

--- src/components/FrontPage.tsx
+++ src/components/FrontPage.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 import Button from '@mui/material/Button';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
